7ff89fe0ad8fdd633574f2caac7ad735e7f093f9
Daniel Diaz <diaz.carrete@facebook.com>
2019-10-26 16:16:54 +0200

    API

--- lib/Piecemeal.hs
+++ lib/Piecemeal.hs
@@ -1,19 +1,40 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeOperators #-}
 
 module Piecemeal where
 
-import Servant.API
+import Data.Aeson
 import Data.RBR
 import Data.SOP
+import Servant
+import Servant.API
 
-newtype Incomplete r = Incomplete { getIncomplete :: Record Maybe (RecordCode r) }
+newtype Incomplete r = Incomplete {getIncomplete :: Record Maybe (RecordCode r)}
 
-newtype Piece r = Piece { getPiece :: Variant I (RecordCode r) }
+newtype Piece r = Piece {getPiece :: Variant I (RecordCode r)}
 
 -- https://docs.servant.dev/en/stable/tutorial/ApiType.html
 type PiecemealAPI r =
   "piecemeal"
     :> ( Get '[JSON] (Either r (Incomplete r))
-           :<|> Patch '[JSON] (Piece r)
+           :<|> ReqBody '[JSON] (Piece r) :> Patch '[JSON] ()
        )
+
+-- https://hackage.haskell.org/package/servant-0.4.2/docs/Servant-API-Patch.html
+--  _foo1 :: Handler (Either r (Incomplete r))
+--  _foo2 :: Piece r -> Handler ()
+piecemealApp ::
+  forall r.
+  (ToJSON r, ToJSON (Incomplete r), FromJSON (Piece r)) =>
+  Proxy r ->
+  Application
+piecemealApp _ = serve (Proxy @(PiecemealAPI r)) (_foo1 :<|> _foo2)
